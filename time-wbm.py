891278842f933892d1bd1a8d147b92d1c005baa7
Nikhil Bhat <nbhat15@gsb.columbia.edu>
2013-05-15 10:48:58 -0400

    time-wbm.py changed

--- time-wbm.py
+++ time-wbm.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 
 from subprocess import call
+import sys
 import time
 
 start_time = time.time()
-call(["./wbm.out"])
+args = sys.argv
+args[0] = "./wbm.out"
+call(args)
 print "execution took ",time.time() - start_time, "seconds"
